266acd3dd3ebbf3ff47a520c8cd04f571e6d2336
Александр Лебедев <allebedev@samokat.ru>
2023-12-22 11:55:49 +0300

    Delete peer hostname label and resolve logic

--- internal/collector/metrics.go
+++ internal/collector/metrics.go
@@ -2,10 +2,7 @@ package collector
 
 import (
 	"fmt"
-	"log"
 	"strings"
-
-	lnet "github.com/alebsys/ngraph/internal/localnetinfo"
 )
 
 const (
@@ -15,13 +12,8 @@ const (
 
 func createMetric(k string, v int) (string, error) {
 	connMeta := strings.Split(k, "-")
-	peerHostName, err := lnet.ResolveAddr(connMeta[1])
-	if err != nil {
-		log.Printf("error: the address %s was not resolved", connMeta[1])
-	}
-
 	if connMeta[2] == "output" {
-		return fmt.Sprintf("%s{src_ip=\"%s\", dest_ip=\"%s\", peer_hostname=\"%s\"} %d\n", outputMetricName, connMeta[0], connMeta[1], peerHostName, v), nil
+		return fmt.Sprintf("%s{src_ip=\"%s\", dest_ip=\"%s\"} %d\n", outputMetricName, connMeta[0], connMeta[1], v), nil
 	}
-	return fmt.Sprintf("%s{src_ip=\"%s\", dest_ip=\"%s\", peer_hostname=\"%s\"} %d\n", inputMetricName, connMeta[1], connMeta[0], peerHostName, v), nil
+	return fmt.Sprintf("%s{src_ip=\"%s\", dest_ip=\"%s\"} %d\n", inputMetricName, connMeta[1], connMeta[0], v), nil
 }
--- internal/localnetinfo/localnetinfo.go
+++ internal/localnetinfo/localnetinfo.go
@@ -1,26 +1,10 @@
 package localnetinfo
 
 import (
-	"net"
 	"os"
 	"strconv"
 	"strings"
 )
-
-// TODO: проверить какое значение из slice следует возвращать.
-func ResolveAddr(addr string) (string, error) {
-	hostnames, err := net.LookupAddr(addr)
-	if err != nil {
-		return "", err
-	}
-	hostname := hostnames[0]
-
-	// return hostname without last dot symbol
-	if len(hostname) > 0 {
-		hostname = hostname[:len(hostnames)-1]
-	}
-	return hostname, nil
-}
 
 func GetPortRange(file string) (int, int, error) {
 	data, err := os.ReadFile(file)
